e2bd1d831e0a47cf62f060dd564cdbded82bc160
kamaukimani <paulkimys285@gmail.com>
2026-07-04 13:17:18 +0300

    render the App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import App from './components/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>Hello.....Lets learn something today!!!</h1>
+    {/* <h1>Hello.....Lets learn something today!!!</h1> */}
+    <App/>
   </StrictMode>,
 )
